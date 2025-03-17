c622e806c92342489fa09705e656a4574b4eb2b9
SamuraiWarmonger <misc.samchris@gmail.com>
2025-03-17 22:15:17 +0530

    Test Basic tutorial

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/test-game')
+def test_game():
+    return render_template('game.html')
+
 if __name__ == '__main__':
     app.run(debug=True) 
